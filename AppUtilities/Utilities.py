efeb1704ffbef5e4166afd8fc61fb408de970b66
Sneezedip <97994282+Sneezedip@users.noreply.github.com>
2024-01-12 10:00:38 -0100

    Add files via upload

--- AppUtilities/Utilities.py
+++ AppUtilities/Utilities.py
@@ -47,9 +47,9 @@ class Utilities():
             case "bar": return next(item["text"] for item in bar_translations if item["id"] == id)
     def LoadIcons(root):
         steamicon = customtkinter.CTkLabel(root,height=18,width=18,image=customtkinter.CTkImage(Utilities.GetImages("https://img.icons8.com/external-those-icons-lineal-color-those-icons/24/external-Steam-social-media-those-icons-lineal-color-those-icons.png"), size=(18, 18)),text="",bg_color="#171d25").place(x=9,y=9)
-        closebutton = customtkinter.CTkLabel(root,height=18,width=18,image=customtkinter.CTkImage(Utilities.GetImages("https://img.icons8.com/material-two-tone/24/delete-sign.png"), size=(18, 18)),text="",bg_color="#171d25")
-        fullscreenbutton = customtkinter.CTkLabel(root,height=18,width=18,image=customtkinter.CTkImage(Utilities.GetImages("https://img.icons8.com/ios/50/full-screen--v1.png"), size=(18, 18)),text="",bg_color="#171d25").place(x=945,y=5)
-        minimizebutton = customtkinter.CTkLabel(root,height=18,width=18,image=customtkinter.CTkImage(Utilities.GetImages("https://img.icons8.com/ios-glyphs/30/horizontal-line.png"), size=(18, 18)),text="",bg_color="#171d25").place(x=910,y=5)
+        closebutton = customtkinter.CTkLabel(root,height=18,width=18,image=customtkinter.CTkImage(Utilities.GetImages("https://cdn-icons-png.flaticon.com/512/10728/10728089.png"), size=(18, 18)),text="",bg_color="#171d25")
+        fullscreenbutton = customtkinter.CTkLabel(root,height=18,width=18,image=customtkinter.CTkImage(Utilities.GetImages("https://img.icons8.com/external-dreamstale-lineal-dreamstale/32/external-expand-arrows-dreamstale-lineal-dreamstale-6.png"), size=(18, 18)),text="",bg_color="#171d25").place(x=945,y=5)
+        minimizebutton = customtkinter.CTkLabel(root,height=18,width=18,image=customtkinter.CTkImage(Utilities.GetImages("https://img.icons8.com/ios-filled/50/737373/minus-math.png"), size=(18, 18)),text="",bg_color="#171d25").place(x=910,y=5)
         #user
         userphoto = customtkinter.CTkLabel(root,height=25,width=25,image=customtkinter.CTkImage(Utilities.GetImages("https://th.bing.com/th/id/OIP.MaDrjtmPQGzKiLHrHEPfFAHaHa?rs=1&pid=ImgDetMain"), size=(25, 25)),text="",bg_color="#171d25").place(x=745,y=5)
         userbackground = customtkinter.CTkFrame(root,height=25,width=120,bg_color="#272d37",fg_color="#272d37").place(x=770,y=5)
@@ -64,6 +64,19 @@ class Utilities():
         friendsText = customtkinter.CTkLabel(root,text=Utilities.Translations("bar",2),font=("Motiva Sans",13),bg_color="#171d25",text_color="#858c95")
         gamesText = customtkinter.CTkLabel(root,text=Utilities.Translations("bar",3),font=("Motiva Sans",13),bg_color="#171d25",text_color="#858c95")
         helpText = customtkinter.CTkLabel(root,text=Utilities.Translations("bar",4),font=("Motiva Sans",13),bg_color="#171d25",text_color="#858c95")
+
+        #CONFIG HOVERING COLORS
+        steamText.bind("<Enter>", lambda event: event.widget.config(fg="white"))
+        steamText.bind("<Leave>", lambda event: event.widget.config(fg="#858c95"))
+        seeText.bind("<Enter>", lambda event: event.widget.config(fg="white"))
+        seeText.bind("<Leave>", lambda event: event.widget.config(fg="#858c95"))
+        friendsText.bind("<Enter>", lambda event: event.widget.config(fg="white"))
+        friendsText.bind("<Leave>", lambda event: event.widget.config(fg="#858c95"))
+        gamesText.bind("<Enter>", lambda event: event.widget.config(fg="white"))
+        gamesText.bind("<Leave>", lambda event: event.widget.config(fg="#858c95"))
+        helpText.bind("<Enter>", lambda event: event.widget.config(fg="white"))
+        helpText.bind("<Leave>", lambda event: event.widget.config(fg="#858c95"))
+        #CONFIG HOVERING COLORS
 
         steamText.place(x=35,y=4)
         seeText.place(x=100,y=4)
